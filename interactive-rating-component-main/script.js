4e5443023bcfe908a7060154adf5fbf1bb720870
ankit-345 <109626032+ankit-345@users.noreply.github.com>
2023-03-22 22:10:24 +0530

    Update script.js

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -7,8 +7,11 @@ const thankBody = document.querySelector('.thank-you-body');
 const thankFooter = document.querySelector('.thank-you-footer');
 const rate = document.querySelector('.rate');
 const thankFooterbtn = document.querySelector('.thank-you-footer-btn');
-var value;
 
+const card = document.querySelector('.card');
+
+submit_btn.disabled = true;
+ 
 submit_btn.addEventListener('click', () => {
     cardHeader.classList.add('hidden');
     cardBody.classList.add('hidden');
@@ -16,20 +19,32 @@ submit_btn.addEventListener('click', () => {
     thankBody.classList.remove('hidden');
     thankFooter.classList.remove('hidden');
     thankFooterbtn.classList.remove('hidden');
+
+    card.classList.add('open');
+    card.classList.remove('close');
+
 })
 
+
 thankFooterbtn.addEventListener('click', () => {
+    
     cardHeader.classList.remove('hidden');
     cardBody.classList.remove('hidden');
     cardFooter.classList.remove('hidden');
     thankBody.classList.add('hidden');
     thankFooter.classList.add('hidden');
     thankFooterbtn.classList.add('hidden');
+    
+    card.classList.remove('open');
+    card.classList.add('close');
+
+    submit_btn.disabled = true;
 })
 
 btns.forEach(btn=> {
     btn.addEventListener('click', () => {
         rate.innerHTML = btn.innerHTML;
+        submit_btn.disabled = false;
     })
 })
 
